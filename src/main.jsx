d4727258e883156e7b3f4b5bf5cc165d9aebab10
LautaroLeall <lautaroleal4@gmail.com>
2025-09-22 14:06:54 -0300

    create files for project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/global.css'
 
-export default function main() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
